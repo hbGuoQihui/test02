31982803adc1ad8eaa6da31402a99ae1dc03f591
aaaaa <aaaaa@qq.com>
2022-08-05 15:24:12 +0800

    8888888888
    # Conflicts:
    #	src/com/sjzxy/Test01.java

--- src/com/sjzxy/Test01.java
+++ src/com/sjzxy/Test01.java
@@ -1,10 +1,12 @@
 package com.sjzxy;
 
+
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("22222");
         System.out.println("33333");
         System.out.println("44444");
+        System.out.println("55555");
     }
 }
